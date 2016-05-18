a113a8c26144f54c840632d0e7b4ddec61bc19f3
Greg Karp <gkarp27@gmail.com>
2016-05-18 01:39:31 +0000

    Steve Greg and Alex first push

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
 """This is the entry point of the program."""
 
-from .languages import LANGUAGES
-
+from languages import LANGUAGES
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    textlist = text.split()
+    result = {}
+    for language in LANGUAGES:
+        match = len([ x for x in textlist if x in language['common_words']])
+        result[match] = language['name']
+    mylang = result[max(result.keys())]
+    return mylang
+    
+#detect_language(text,LANGUAGES)
+
+
+
+
